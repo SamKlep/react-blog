a036da9d18dbc79b90b333167f3acf4800d2aec0
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-13 23:47:48 -0500

    Added react-router-dom and single blog view.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Posts from "./components/Posts";
+import Post from "./components/Post";
 
 class App extends Component {
   state = {
     posts: [
       {
         id: 1,
+        slug: "hello-react",
         title: "Hello React",
         content: "Lorem.",
       },
       {
         id: 2,
+        slug: "hello-project",
         title: "Hello Project",
         content: "Tothe.",
       },
       {
         id: 3,
+        slug: "hello-blog",
         title: "Hello Blog",
         content: "Ipsum.",
       },
@@ -26,10 +31,27 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        <Header />
-        <Posts posts={this.state.posts} />
-      </div>
+      <Router>
+        <div className='App'>
+          <Header />
+          <Switch>
+            <Route
+              exact
+              path='/'
+              render={() => <Posts posts={this.state.posts} />}
+            />
+            <Route
+              path='/post/:postSlug'
+              render={(props) => {
+                const post = this.state.posts.find(
+                  (post) => post.slug === props.match.params.postSlug
+                );
+                return <Post post={post} />;
+              }}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
